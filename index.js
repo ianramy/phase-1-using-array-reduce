62274328d40eecc086e82205ba4ce9d8033a64a2
ianramy <imwagore@gmail.com>
2024-07-19 04:53:10 +0300

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Calculate total batteries
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+// Define initial state
+const initialState = {
+  totalBatteries: 0,
+};
+
+// Reducer function
+function batteriesReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_BATTERIES':
+      return {
+        totalBatteries: state.totalBatteries + action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+// Export the reducer
+module.exports = batteriesReducer;
+
+// Set totalBatteries globally for tests
+global.totalBatteries = totalBatteries;
